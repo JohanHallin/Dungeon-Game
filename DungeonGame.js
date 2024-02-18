dd42ec8858f09221256bf15c76cea38a353615e0
JohanHallin <johan@aoshima.se>
2024-02-18 11:07:24 +0100

    random room reneration & screen divided into 9 equal parts

--- DungeonGame.js
+++ DungeonGame.js
@@ -1,1 +1,22 @@
+class room{
+    constructor(height, width){
+        this.height = height;
+        this.width = width;
+    }
+}
+var rooms = [];
+for(i = 0; i < 9; i++){
+    newRoom = new room(rnd(3.0, 10.0), rnd(3.0, 10.0));
+    rooms.push(newRoom);
+}
 
+function regenerateRoom(){
+    for(i=0; i < 9; i++){
+        rooms[i].height = rnd(3, 10);
+        rooms[i].width = rnd(3, 10);
+    }
+}
+
+function rnd(min, max){
+    return Math.round(min + (max - min) * Math.random()); 
+}
