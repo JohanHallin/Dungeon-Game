867f7ba83df856fdf15443ea47f7b58a9b6585ae
JohanHallin <johan@aoshima.se>
2024-02-28 09:48:03 +0100

    Cleaned up code

--- DungeonGame.js
+++ DungeonGame.js
@@ -24,9 +24,18 @@ function rnd(in1, in2){
     }
 }
 
+// Startup function
+// Calls alls functions that need to run at start
+function startUp(){
+    generateRooms();
+    selectStartingRoom();
+    canvas.style.background = "#888";
+    onresize();
+}
+
 // Debug settings
 // False by default 
-var seeAll = false;
+var seeAll = true;
 
 // Map settings
 var layout = new Vector2(5, 3);
@@ -35,33 +44,29 @@ var roomMinSize = new Vector2(5, 5);
 var roomMaxSize = new Vector2(10, 10);
 var wallThickness = 1;
 var startingRoom;
-selectStartingRoom();
 function selectStartingRoom(){
     startingRoom = new Vector2(rnd(layout.x), rnd(layout.y));
-    console.log(startingRoom);
 }
 
 // Map data
-var rooms = [];
-for(var x = 0; x < layout.x; x++){
-    rooms.push(new Array());
-    for(var y = 0; y < layout.y; y++){
-        rooms[x].push(new Room(new Vector2(roomMargin + x * (2 * roomMargin + roomMaxSize.x), roomMargin + y * (2 * roomMargin + roomMaxSize.y)), new Vector2(rnd(roomMinSize.x, roomMaxSize.x), rnd(roomMinSize.y, roomMaxSize.y))));
+var rooms;
+function generateRooms(){
+    rooms = [];
+    for(var x = 0; x < layout.x; x++){
+        rooms.push(new Array());
+        for(var y = 0; y < layout.y; y++){
+            rooms[x].push(new Room(new Vector2(roomMargin + x * (2 * roomMargin + roomMaxSize.x), roomMargin + y * (2 * roomMargin + roomMaxSize.y)), new Vector2(rnd(roomMinSize.x, roomMaxSize.x), rnd(roomMinSize.y, roomMaxSize.y))));
+        }
     }
 }
-console.log(rooms);
 
 // Canvas
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
-canvas.style.background = "#888";
 
 // pixelSize used to convert coordinates into pixels depending on screen size
 var pixelSize;
-recalculatePixelSize();
-function recalculatePixelSize(){
+function calculatePixelSize(){
     pixelSize = new Vector2(canvas.width / ((2 * roomMargin + roomMaxSize.x) * layout.x), canvas.height / ((2 * roomMargin + roomMaxSize.y) * layout.y));
 }
 
@@ -70,7 +75,7 @@ function recalculatePixelSize(){
 window.onresize = function(ev) { 
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-    recalculatePixelSize();
+    calculatePixelSize();
     draw();
 }
 
